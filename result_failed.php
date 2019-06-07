8da1b11d71cc7b99dad4cd5b61aa35bb4c3a0bc1
bluedisk <bluedisk@gmail.com>
2019-06-08 00:06:41 +0900

    에러 처리 예제 추가

--- result_failed.php
+++ result_failed.php
@@ -13,7 +13,39 @@
 </header>
 <article>
     <h1>요청 실패!</h1>
-    결과 코드: <?= $_GET['code'] ?>
+    <?php
+    echo "결과 코드: " . $_GET['code'] . " = ";
+
+    switch ($_GET['code']) {
+        case "7010":
+            echo "받을 대상자 존재 하지 않음";
+            break;
+        case "7060":
+            echo "받을 대상자의 지갑이 비활성화 되어 있음";
+            break;
+        case "7090":
+            echo "받을 대상자의 인증이 완료 되지 않은 상태";
+            break;
+        case "6010":
+            echo "보낼 대상자의 지갑이 비활성화 되어 있음";
+            break;
+        case "6060":
+            echo "보낼 대상자의 인증이 완료 되지 않은 상태";
+            break;
+        case "6090":
+            echo "보낼 대상자의 처리 가능 수량 초과";
+            break;
+        case "5050":
+            echo "보내기 처리중 에러";
+            break;
+        case "5060":
+            echo "자신의 지갑으로 전송시";
+            break;
+        case "5070":
+            echo "sms 인증 문자 불일치";
+            break;
+    }
+    ?>
 </article>
 </body>
 </html>
